6c5163bf2180043eb90d0c6b6fd00805a227b47b
Krzysztof Boronski <kboronski@antmicro.com>
2025-01-21 15:00:09 +0100

    Remove leftover files from previous version of "counter" example
    Info in counter.md is no longer inaccurate.
    
    Signed-off-by: Krzysztof Boronski <kboronski@antmicro.com>

--- examples/counter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2025 Antmicro <www.antmicro.com>
-# SPDX-License-Identifier: Apache-2.0
-
-iverilog -lcounter.v counter_tb.v -ocounter.out
-./counter.out
-cargo run --release -- --clk-freq 1e9 counter.vcd
