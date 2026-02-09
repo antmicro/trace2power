d08f851537360611b9797d1c97733977d299249e
Bartłomiej Chmiel <bchmiel@antmicro.com>
2026-02-09 09:12:10 +0100

    Tests
    Signed-off-by: Bartłomiej Chmiel <bchmiel@antmicro.com>

--- examples/limit_scope_power/limit_scope_power.sh
+++ examples/limit_scope_power/limit_scope_power.sh
@@ -10,22 +10,12 @@ iverilog -g2012 -lhierarchical.sv hierarchical_tb.sv -ohierarchical_tb
 # Run simulation and generate a VCD trace file
 ./hierarchical_tb
 
-# Process the VCD file to SAIF
-trace2power \
-    --clk-freq 500000000 \
-    --output-format saif \
-    --limit-scope hierarchical_tb \
-    --limit-scope-power hierarchical_tb.dut.adder1 \
-    --ignore-date \
-    --ignore-version \
-    --output out.saif \
-    hierarchical.vcd
-
 # Process the VCD file to TCL
 trace2power \
     --clk-freq 500000000 \
     --output-format tcl \
     --limit-scope hierarchical_tb \
     --limit-scope-power hierarchical_tb.dut.adder1 \
+    --input-ports-activity \
     --output out.tcl \
     hierarchical.vcd
---
+++ tests/limit_scope_power.rs
@@ -0,0 +1,65 @@
+// Copyright (c) 2024-2026 Antmicro <www.antmicro.com>
+// SPDX-License-Identifier: Apache-2.0
+
+use std::io::Read;
+use tempfile::NamedTempFile;
+
+use libtrace2power::Args;
+use libtrace2power::OutputFormat;
+use libtrace2power::process;
+use std::path::PathBuf;
+
+fn common_args(output_file: &NamedTempFile) -> Args {
+    Args {
+        input_file: PathBuf::from(r"tests/limit_scope_power/hierarchical.vcd"),
+        clk_freq: 500000000.0,
+        clock_name: None,
+        output_format: OutputFormat::Tcl,
+        limit_scope: Some(String::from("hierarchical_tb")),
+        netlist: None,
+        top: None,
+        top_scope: None,
+        blackboxes_only: false,
+        remove_virtual_pins: true,
+        output: Some(output_file.path().to_path_buf()),
+        ignore_date: false,
+        ignore_version: false,
+        per_clock_cycle: false,
+        only_glitches: false,
+        export_empty: false,
+        limit_scope_power: Some(String::from("hierarchical_tb.dut.adder1")),
+        input_ports_activity: true,
+    }
+}
+
+#[test]
+fn test_limit_scope_power_tcl() {
+    let mut output_file = NamedTempFile::new().expect("Failed to allocate temp file");
+    let args = common_args(&output_file);
+
+    process(args);
+
+    let golden = include_str!("limit_scope_power/limit_scope_power.tcl");
+    let mut actual = String::new();
+    output_file
+        .read_to_string(&mut actual)
+        .expect("Actual file should exist");
+    assert_eq!(sort_tcl(actual), sort_tcl(String::from(golden)));
+}
+
+// TODO remove once grouped_stats would be iterated in a deterministic way
+fn sort_tcl(input: String) -> String {
+    let mut lines: Vec<&str> = input.lines().collect();
+
+    fn key(s: &str) -> Vec<u8> {
+        s.bytes()
+            // ignore leading blanks
+            .skip_while(|&c| c == b' ' || c == b'\t')
+            // dictionary order
+            .filter(|&c| c.is_ascii_alphanumeric() || c == b' ' || c == b'\t')
+            .collect()
+    }
+
+    lines.sort_by(|a, b| key(a).cmp(&key(b)));
+    lines.join("\n")
+}
